1182367cb720ade95451e7fe889ef06688ef6e56
Eleanor Maynard <el_mynrd@sbcglobal.net>
2020-02-29 17:40:39 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('BACON DOUBLE CHEESEBURGER');
+INSERT INTO burgers (burger_name) VALUES ('TOFU BURGER');
+INSERT INTO burgers (burger_name) VALUES ('BARBECUE CHEESEBURGER');
